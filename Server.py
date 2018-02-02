44f1f5e13f3dfeabd7cad29c3c34ab27b3f00485
EC2 Default User <ec2-user@ip-172-31-20-134.us-west-2.compute.internal>
2018-02-02 20:21:34 +0000

    added functions to server.py, still not done though

--- Server.py
+++ Server.py
@@ -14,29 +14,76 @@ import pickle
 import socket
 import sys
 
-# Create a socket
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+open_files = {}
 
-# Associate socket with a port
-host = ''
-port = 1338
-s.bind((host, port))
+def parse_command(x):
+    if (x.cmd == 'r'):
+    elif (x.cmd == 'w'):
+    elif (x.cmd == 'o'):
+        return file_open(x)
+    elif ()
+    
+def file_open(x):
+    open_files[x.filename] = open(x.filename)
+    return x
+    
+def file_write(x):
+    return x
 
-# accept "call" from client
-s.listen(1)
+def file_read(x):
+    if x.data == -1:
+        x.data = open_files[x.file_name].read()
+    else:
+        x.data = open_file[x.file_name].read(x.data)
+    return x
+    
+def file_close(x):
+    open_files[x.filename].close()
+    del open_files[x.filename]
+    return x
+    
+def close_all(conn):
+    conn.close()
+    for file in open_files:
+        file.close()
+    
 
-conn, addr = s.accept()
-print 'Client is at', addr
+def main():
+    # Create a socket
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    
+    # Associate socket with a port
+    host = ''
+    port = 1338
+    s.bind((host, port))
+    
+    
+    
+    
+    while(1):
+        # accept "call" from client
+        s.listen(1)
+        
+        conn, addr = s.accept()
+        print 'Client is at', addr
+        
+        mf = conn.makefile()
+        x = pickle.load(mf)
+        mf.close()
+        
+        if x.cmd == 'k':
+            close_all(conn)
+            break
+        
+        # Parse Command
+        x = parse_command(x)
+            
+        
+        # Send Back Packet
+        mf = conn.makefile()
+        pickle.dump(x, mf)
+        
+        conn.close()
 
-mf = conn.makefile()
-x = pickle.load(mf)
-mf.close()
 
-# Send Back Packet
-mf = conn.makefile()
-f = open(x.filename)
-x.data = f.read()
-
-pickle.dump(x, mf)
-
-conn.close()
+if __name__ == '__main__': main()
