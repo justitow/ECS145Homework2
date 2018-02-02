d11d4d21c14d62aadf2c8f2c68fc98ca1bd8efed
EC2 Default User <ec2-user@ip-172-31-20-134.us-west-2.compute.internal>
2018-02-02 20:29:40 +0000

    added manager packet sending

--- Manager.py
+++ Manager.py
@@ -1,5 +1,12 @@
 import os, socket, sys
 
+class packet:
+    def __init__(self, name, command, data):
+        self.filename = name
+        self.cmd = command
+        self.data = data
+
+
 path = './ECS145/HW2/'
 
 class Manager:
@@ -20,7 +27,11 @@ class Manager:
         s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         for host in hostList:
             s.connect((host, self.port))
-            s.send('q')
+            
+            
+            mf = s.makefile()
+            pickle.dump(packet('', 'k', ''), mf)
+            mf.close()
             s.close()
 
 
